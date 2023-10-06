35d2749b4af00637d99be9dbe700dc28bc26e5d0
Hudson S <hudsonshaeffer@gmail.com>
2023-10-06 09:21:38 -0400

    lvlaue and rvalue parse branches added, should work. added assertequals

--- Lexer.rb
+++ Lexer.rb
@@ -2,9 +2,6 @@ require_relative "Token.rb"
 include Tokens
 
 module Lexer
-    def initialize
-    end
-
     def lex(expression)
 
         # loop variables
@@ -40,12 +37,6 @@ module Lexer
             $token_count += 1
             $token_source = ''
         end
-
-        # Check if the current character is a character (ignoring case)
-        def is_char; $i < $expression.length && $expression[$i] === /[a-z]/i; end;
-        
-        # Check if current character is a valid string char.
-        def is_str; $i < $expression.length && $expression[$i] === /\w/; end;
         
         # Check if current character is a valid boolean char.
         def is_first_true; $i < $expression.length && $expression[$i] === "t"; end; # true
@@ -58,123 +49,165 @@ module Lexer
         def is_fourth_false; $i < $expression.length && $expression[$i] === "s"; end;
         def is_fifth_false; $i < $expression.length && $expression[$i] === "e"; end;
         
-        # Check if current character is a digit.
-        def is_digit; $i < $expression.length && $expression[$i] === /\d/; end;
-
-        # Check if the current character is a dot
-        def is_decimal; $i < $expression.length && $expression[$i] === "."; end;
+        # Check if current character is a digit, dot, or comma
+        def is_digit; $i < $expression.length && "0" <= $expression[$i] && $expression[$i] <= "9"; end;
+        def is_decimal; $i < $expression.length && $expression[$i] == "."; end;
+        def is_comma; $i < $expression.length && $expression[$i] == ","; end;
 
         # Check if the current character is a quote
-        def is_quote; $i < $expression.length && $expression[$i] === '"'; end;
+        def is_quote; $i < $expression.length && $expression[$i] == '"'; end;
+        
+        # Check if the current character is an ampersand
+        def is_ampersand; $i < $expression.length && $expression[$i] == '&'; end;
+        
+        # Check if the current character is an open bracket, close bracket, open parenthesis, or close parenthesis
+        def is_open_bracket; $i < $expression.length && $expression[$i] == '['; end;
+        def is_close_bracket; $i < $expression.length && $expression[$i] == ']'; end;
+        def is_open_parenthesis; $i < $expression.length && $expression[$i] == '('; end;
+        def is_close_parenthesis; $i < $expression.length && $expression[$i] == ')'; end;
 
         # Check if the current character is a whitespace
-        def is_whitespace; $i < $expression.length && $expression[$i] === " "; end;
+        def is_whitespace; $i < $expression.length && $expression[$i] == " "; end;
 
         # lexing loop
         while ($i < $expression.length)
-            if is_whitespace # ---------------------------- Whitespace (noop) Branch
+            if is_whitespace # ------------------------------- Whitespace (noop) Branch
                 abandon
 
             elsif is_quote # --------------------------------- String Primitive Branch
                 $start_index = $i
                 capture
-                while !is_quote # after first quote, loop until another quote is detected
+                while !is_quote # munch all chars up until the closing quote
                     capture
                 end
-                # once closing quote is found capture it again
+                # munch closing quote
                 $end_index = $i
                 capture
-                # emit finished string token
                 $token_type = :string
-                emitToken 
+                emitToken # emit string token
 
             elsif is_digit # --------------------------------- Float/Integer Primitives Branch
                 $start_index = $i
                 capture
-                while is_digit # after first digit is found, loop through all digits
+                while is_digit # munch all consecutive digits
                     $end_index = $i
                     capture
                 end
                 if is_decimal # ------------------- Floats SubBranch
                     capture
-                    while is_digit # loop through all remaining digits
+                    while is_digit # munch all consecutive digits
                         $end_index = $i
                         capture
+                        $token_type = :float
+                        emitToken # emit float token
                     end
-                    $token_type = :float
-                    emitToken
                 elsif is_whitespace # ------------- Integer SubBranch
                     $token_type = :integer
-                    emitToken
-                else # ---------------------------- Bad number subbranch :(
-                    abandon
-                end
+                    emitToken #emit integer token
+                else; abandon; end
 
             elsif is_first_true # ---------------------------- True Branch
                 $start_index = $i
                 capture
-                if is_second_true
+                if is_second_true # munch r
                     capture
-                    if is_third_true
+                    if is_third_true # munch u
                         capture
-                        if is_fourth_true
+                        if is_fourth_true # munch e
                             $end_index = $i
                             capture
-                        else
-                            abandon
-                        end
-                    else
-                        abandon
-                    end
-                else
-                    abandon
-                end
+                        else; abandon; end
+                    else; abandon; end
+                else; abandon; end
                 # once word is complete, if its followed by a whitespace or the end of the $expression then its a boolean primitive value
                 if !($i < $expression.length) || is_whitespace
                     $token_type = :boolean
-                    emitToken
-                else
-                    $token_type = :invalid_token
-                    abandon
-                end
+                    emitToken # emit boolean token
+                else; abandon; end
 
             elsif is_first_false # --------------------------- False Branch
                 $start_index = $i
                 capture
-                if is_second_false
+                if is_second_false # munch a
                     capture
-                    if is_third_false
+                    if is_third_false # munch l
                         capture
-                        if is_fourth_false
+                        if is_fourth_false # munch s
                             capture
-                            if is_fifth_false
+                            if is_fifth_false # munch e
                                 $end_index = $i
                                 capture
-                            else
-                                abandon
-                            end
-                        else
-                            abandon
-                        end
-                    else
-                        abandon
-                    end
-                else
-                    abandon
-                end
+                            else; abandon; end
+                        else; abandon; end
+                    else; abandon; end
+                else; abandon; end
                 # once word is complete, if its followed by a whitespace or the end of the $expression then its a boolean primitive value
                 if !($i < $expression.length) || is_whitespace
                     $token_type = :boolean
-                    emitToken
-                else
-                    $token_type = :invalid_token
-                    abandon
-                end
+                    emitToken # emit boolean token
+                else; abandon; end
 
+            elsif is_ampersand # ----------------------------- Lvalue Branch
+                $start_index = $i
+                capture
+                if is_open_bracket # munch open bracket
+                    capture
+                    while is_whitespace; skip; end # munch variable amount of whitespace [ num
+                    if is_digit # munch first x value digit
+                        capture
+                        while is_digit # munch rest x digits
+                            capture
+                        end
+                        while is_whitespace; skip; end # munch variable amount of whitespace num ,
+                        if is_comma # munch comma delimiter
+                            capture
+                            while is_whitespace; skip; end # munch variable amount of whitespace , num
+                            if is_digit # munch first y value digit
+                                capture
+                                while is_digit # munch rest y digits
+                                    capture
+                                end
+                                while is_whitespace; skip; end # munch variable amount of whitespace num ]
+                                if is_close_bracket # munch close bracket
+                                    $end_index = $i
+                                    capture
+                                    $token_type = :lvalue
+                                    emitToken # emit lvalue token
+                                else; abandon; end
+                            else; abandon; end
+                        else; abandon; end
+                    else; abandon; end
+                else; abandon; end
+            elsif is_open_bracket # -------------------------- Rvalue Branch
+                $start_index = $i
+                capture
+                while is_whitespace; skip; end # munch variable amount of whitespace [ num
+                if is_digit # munch first x value digit
+                    capture
+                    while is_digit # munch rest x digits
+                        capture
+                    end
+                    while is_whitespace; skip; end # munch variable amount of whitespace num ,
+                    if is_comma # munch comma delimiter
+                        capture
+                        while is_whitespace; skip; end # munch variable amount of whitespace , num
+                        if is_digit # munch first y value digit
+                            capture
+                            while is_digit # munch rest y digits
+                                capture
+                            end
+                            while is_whitespace; skip; end # munch variable amount of whitespace num ]
+                            if is_close_bracket # munch close bracket
+                                $end_index = $i
+                                capture
+                                $token_type = :rvalue
+                                emitToken # emit rvalue token
+                            else abandon; end
+                        else; abandon; end
+                    else; abandon; end
+                else; abandon; end
             elsif false
-            elsif false
-            else # ------------------------------------------ Default Branch
-                p "reached default"
+            else # ------------------------------------------- Default Branch
                 $i += 1
             end
         end
@@ -182,3 +215,4 @@ module Lexer
         $tokens
     end
 end
+
--- LexerTests.rb
+++ LexerTests.rb
@@ -1,3 +1,12 @@
 require_relative "Lexer.rb"
+require_relative "Token.rb"
 include Lexer
+include Tokens
 
+def assert_equal(expected, actual, message)
+    if !expected.equals?(actual)
+        puts "#{message}. Expected: #{expected}   Actual: #{actual}"
+    end
+end
+
+puts lex("&[4, 1]")[0].to_s
