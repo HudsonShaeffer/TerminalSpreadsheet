a1c492fc449bf447a3c0237b0fd27e3939455938
Hudson S <hudsonshaeffer@gmail.com>
2023-10-06 09:31:43 -0400

    stubbed out tester and run_tests

--- LexerTests.rb
+++ LexerTests.rb
@@ -9,4 +9,20 @@ def assert_equal(expected, actual, message)
     end
 end
 
+class LexerTester
+    def initialize
+    end
+
+    def test_lex
+
+    end
+end
+
+def run_tests(testee)
+    testClass = testee.new; methods = testClass.methods; testMethods = methods.filter { |name| name.to_s.include? "test_" }
+    testMethods.each { |method| testClass.send(method) }
+    puts 
+end 
+run_tests(LexerTester)
+
 puts lex("&[4, 1]")[0].to_s
