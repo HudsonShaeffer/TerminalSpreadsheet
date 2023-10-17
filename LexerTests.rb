e5dee420a1f767fe762dc054db7226653d501d0a
HudsonS <hudsonshaeffer@gmail.com>
2023-10-17 09:51:29 -0400

    ooga

--- LexerTests.rb
+++ LexerTests.rb
@@ -14,42 +14,42 @@ class LexerTester
     end
 
     def test_lex
-        # puts "\nString Primitive:\n"
-        # lex("\"Hello World.\"").each() { |token| token.to_s}
-        # puts "\nInteger & Float Primitives:\n"
-        # lex("15 -15 0 1.5 -1.5").each() { |token| token.to_s}
-        # puts "\nBoolean Primitives:\n"
-        # lex("truefalseTrueFalse").each() { |token| token.to_s}
-        # puts "\nKeywords:\n"
-        # lex("maxminmeansum float int").each() { |token| token.to_s}
-        # puts "\nDelimiters:\n"
-        # lex("($[,],$[,])").each() { |token| token.to_s}
-        # puts "\nOperators:\n"
-        # lex("+-/*%").each() { |token| token.to_s}
-        # lex("==!=").each() { |token| token.to_s}
-        # lex("&|~^&&||!>><<").each() { |token| token.to_s}
-        # lex("< <=> >=").each() { |token| token.to_s}
+        puts "\nString Primitive:\n"
+        lex("\"Hello World.\"").each() { |token| token.to_s}
+        puts "\nInteger & Float Primitives:\n"
+        lex("15 -15 0 1.5 -1.5").each() { |token| token.to_s}
+        puts "\nBoolean Primitives:\n"
+        lex("truefalseTrueFalse").each() { |token| token.to_s}
+        puts "\nKeywords:\n"
+        lex("maxminmeansumfloatint").each() { |token| token.to_s}
+        puts "\nDelimiters:\n"
+        lex("($[,],$[,])").each() { |token| token.to_s}
+        puts "\nOperators:\n"
+        lex("+-/*%").each() { |token| token.to_s}
+        lex("==!=").each() { |token| token.to_s}
+        lex("&|~^&&||!>><<").each() { |token| token.to_s}
+        lex("< <=> >=").each() { |token| token.to_s}
         puts "\nOperations:\n"
-        # lex("5+5").each() { |token| token.to_s}
+        lex("5+5").each() { |token| token.to_s}
         lex("5-5").each() { |token| token.to_s}
-        # lex("5*5").each() { |token| token.to_s}
-        # lex("5/5").each() { |token| token.to_s}
-        # lex("5%5").each() { |token| token.to_s}
-        # lex("5&5").each() { |token| token.to_s}
-        # lex("5|5").each() { |token| token.to_s}
-        # lex("5^5").each() { |token| token.to_s}
-        # lex("~5").each() { |token| token.to_s}
-        # lex("5<<5").each() { |token| token.to_s}
-        # lex("5>>5").each() { |token| token.to_s}
-        # lex("true==true").each() { |token| token.to_s}
-        # lex("true!=true").each() { |token| token.to_s}
-        # lex("true||true").each() { |token| token.to_s}
-        # lex("true&&true").each() { |token| token.to_s}
-        # lex("!true").each() { |token| token.to_s}
-        # lex("5<5").each() { |token| token.to_s}
-        # lex("5<=5").each() { |token| token.to_s}
-        # lex("5>5").each() { |token| token.to_s}
-        # lex("5>=5").each() { |token| token.to_s}
+        lex("5*5").each() { |token| token.to_s}
+        lex("5/5").each() { |token| token.to_s}
+        lex("5%5").each() { |token| token.to_s}
+        lex("5&5").each() { |token| token.to_s}
+        lex("5|5").each() { |token| token.to_s}
+        lex("5^5").each() { |token| token.to_s}
+        lex("~5").each() { |token| token.to_s}
+        lex("5<<5").each() { |token| token.to_s}
+        lex("5>>5").each() { |token| token.to_s}
+        lex("true==true").each() { |token| token.to_s}
+        lex("true!=true").each() { |token| token.to_s}
+        lex("true||true").each() { |token| token.to_s}
+        lex("true&&true").each() { |token| token.to_s}
+        lex("!true").each() { |token| token.to_s}
+        lex("5<5").each() { |token| token.to_s}
+        lex("5<=5").each() { |token| token.to_s}
+        lex("5>5").each() { |token| token.to_s}
+        lex("5>=5").each() { |token| token.to_s}
         puts "\n"
     end
 end
